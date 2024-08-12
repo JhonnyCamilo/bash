8211a52d3b3020e851e09954edb85277825c5b35
JhonnyCamilo <jhonnybeltran57@gmail.com>
2024-08-12 06:09:29 -0500

    bash1 commit

--- test.sh
+++ test.sh
@@ -10,10 +10,11 @@ python3 "$archivo_generar_temperatura" &
 # Guardar el PID del proceso de generación de temperatura
 pid_generar_temperatura=$!
 
-# Esperar unos segundos para asegurar que el archivo de temperatura se ha generado
-sleep 5
+# Bucle infinito para ejecutar el cifrado cada 5 segundos
+while true; do
+    # Ejecutar el script de cifrado
+    python3 "$archivo_cifrado"
 
-# Ejecutar el script de cifrado
-python3 "$archivo_cifrado"
-
-
+    # Esperar 5 segundos antes de la próxima ejecución
+    sleep 5
+done
