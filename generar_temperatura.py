a4bea5a42dd03ea4b220a1d9bfbaeda4ef5193de
JhonnyCamilo <jhonnybeltran57@gmail.com>
2024-08-12 06:06:54 -0500

    bash0 commit

--- generar_temperatura.py
+++ generar_temperatura.py
@@ -4,7 +4,9 @@ import time
 def generar_temperatura():
     while True:
         temperatura = round(random.uniform(-10.0, 40.0), 2)
-        print(f"Temperatura: {temperatura}°C")
+        with open('temperatura.txt', 'w') as file:
+            file.write(f"{temperatura:.2f}")
+        #print(f"Temperatura: {temperatura}°C")
         time.sleep(5)
 
 if __name__ == "__main__":
--- test.sh
+++ test.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Ruta al archivo Python
-archivo_python="temp.py"
+# Ruta a los archivos Python
+archivo_generar_temperatura="generar_temperatura.py"
+archivo_cifrado="simon_cifrado.py"
 
-# Ejecutar el archivo Python
-python3 "$archivo_python"
+# Ejecutar el script de generación de temperatura en segundo plano
+python3 "$archivo_generar_temperatura" &
+
+# Guardar el PID del proceso de generación de temperatura
+pid_generar_temperatura=$!
+
+# Esperar unos segundos para asegurar que el archivo de temperatura se ha generado
+sleep 5
+
+# Ejecutar el script de cifrado
+python3 "$archivo_cifrado"
+
+
